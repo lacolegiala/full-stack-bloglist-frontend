815b0b6af5cc43c89f615e29a7237cf20205b7b7
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-08-12 17:53:29 +0300

    Define proptypes

--- src/components/BlogForm.js
+++ src/components/BlogForm.js
@@ -1,4 +1,5 @@
 import React, {useState} from 'react'
+import PropTypes from 'prop-types'
 
 const BlogForm = ({ createBlog }) => {
   const [title, setTitle] = useState('')
@@ -52,6 +53,10 @@ const BlogForm = ({ createBlog }) => {
       <button type="submit">save</button>
     </form>  
   )
+}
+
+BlogForm.propTypes = {
+  createBlog: PropTypes.func.isRequired,
 }
 
 export default BlogForm
