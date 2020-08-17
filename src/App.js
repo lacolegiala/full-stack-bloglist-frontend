8e72fc8cdf16def421a4fd896c90c1fadbb47c85
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-08-17 16:32:15 +0300

    Add a button to view url and likes

--- src/App.js
+++ src/App.js
@@ -129,7 +129,6 @@ const App = () => {
   }
 
 
-
   return (
     <div>
       <h2>blogs</h2>
@@ -145,7 +144,7 @@ const App = () => {
           </ul>
           {blogForm()}
           {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} />
+            <Blog key={blog.id} blog={blog}/>
           )}
         </div>
       }
--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,10 +1,28 @@
 /* eslint-disable linebreak-style */
 //Fix later
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showAll, setShowAll] = useState(false)
+  const [text, setText] = useState('View')
+
+  const handleClick = () => {
+    setShowAll(!showAll)
+    if (text === 'View') {
+      setText('Hide')
+    }
+    else {
+      setText('View')
+    }
+  }
+
+  return (
+    <div>
+      {blog.title} {blog.author}
+      <button onClick={handleClick}>{text}</button>
+      {showAll === true && <div>{blog.url} {blog.likes}</div>}
+    </div>
+  )
+}
 
 export default Blog
