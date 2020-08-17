8ddb3fa4ec4ef57d40bde94624ac433446ce6dcd
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-08-17 19:19:19 +0300

    Fix the old test and test the view all blog data button

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders the right content', () => {
@@ -16,6 +16,29 @@ test('renders the right content', () => {
   )
 
   expect(component.container).toHaveTextContent(
-    'This is the best blog ever Jane Doe'
+    'Jane Doe' && 'This is the best blog ever'
+  )
+})
+
+test('renders url and likes when the button is clicked', async () => {
+  const blog = {
+    title: 'This is the best blog ever',
+    author: 'Jane Doe',
+    url: 'does.com',
+    likes: 2,
+    user: {
+      username: 'hihii'
+    }
+  }
+
+  const component = render(
+    <Blog blog={blog}></Blog>
+  )
+
+  const button = component.getByText('View all data')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    'does.com' && 2
   )
 })
