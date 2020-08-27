412106309e9d538aecc86f9800b539474dac8b86
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-08-27 17:55:16 +0300

    Sort the blogs according to their likes in descending order

--- src/App.js
+++ src/App.js
@@ -170,13 +170,17 @@ const App = () => {
     )
   }
 
+  const sortedBlogs = [].concat(blogs)
+    .sort((a, b) => a.likes > b.likes ? -1 : 1)
+    .map((blog, id) => 
+      <Blog key={blog.id} blog={blog} handleLike={handleLike} handleRemoveBlog={handleRemove} user={user}></Blog>
+    )
 
   return (
     <div>
       <h2>blogs</h2>
       {message !== '' && <Notification.Notification message={message}></Notification.Notification>}
       {errorMessage !== '' && <Notification.ErrorNotification message={errorMessage}></Notification.ErrorNotification>}
-
       {user === null && loginForm()}
       {user !== null &&
         <div>
@@ -185,9 +189,7 @@ const App = () => {
             <button onClick={logout}>Logout</button>
           </ul>
           {blogForm()}
-          {blogs.map(blog =>
-            <Blog key={blog.id} blog={blog} handleLike={handleLike} handleRemoveBlog={handleRemove} user={user}/>
-          )}
+          {sortedBlogs}
         </div>
       }
     </div>
