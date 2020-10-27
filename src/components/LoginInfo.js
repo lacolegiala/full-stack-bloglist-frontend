ac980e361f370d6cb6fa77b3ff78b64b70db7121
Sofia Riekkola <sofia.riekkola@gmail.com>
2020-10-27 15:10:56 +0200

    Delete commented code

--- src/components/LoginInfo.js
+++ src/components/LoginInfo.js
@@ -7,15 +7,11 @@ import { logoutUser } from '../reducers/loginReducer'
 const LoginInfo = (props) => {
   const history = useHistory()
   const dispatch = useDispatch()
-  // const [username, setUsername] = useState('')
-  // const [password, setPassword] = useState('')
 
   const logout = () => {
     try {
       dispatch(logoutUser())
       dispatch(setNotification('Logged out', 3))
-      // setUsername('')
-      // setPassword('')
       console.log('LOGGING OUT')
     }
     catch (exception) {
